7a09bb1270fa0b6694be8ecd0be2518530873c54
Nick McMillen <nick.mcmillen@student.awesomeinc.org>
2022-04-09 18:07:03 +0000

    blog section ironed out and added cards to portfolio

---
+++ Different Alignment About Me/about-me-align.js
@@ -0,0 +1,39 @@
+// import React from "react";
+// import "./AboutMe.css";
+// import headshot from "./images/headshot.jpg";
+// import { Button, Col, Container, Image, Row } from "react-bootstrap";
+
+// export default function AboutMe() {
+//   return (
+//     // WORK IN PROGRESS FOR REACT BOOTSTRAP
+//     <Container className="container-fluid" className="about-me">
+//       <Row className="col-12">
+//         <Col className="col-6 justify-content-right" id="headshot-column">
+//           {/* HOW DO YOU FLOAT IMAGE TO RIGHT??? */}
+//           <Image
+//             id="headshot"
+//             src={headshot}
+//             // className="img-fluid"
+//             alt="nick-mcmillen-headshot"
+//           />
+//         </Col>
+//         <Col className="col-6 justify-content-center">
+//           Nick McMillen was born in Salem, Oregon to a family of racers. The bug
+//           started early of course but he actually started racing motocross at
+//           the young age of 5. He raced at a serious level until about age 14
+//           when he was unable to continue racing due to the extensive cost and
+//           time his parents had to take off in order to make it to the national
+//           races and help progress his career. He knew he wanted to do something
+//           in the field of motorsports so if Nick couldn’t be the racer, he at
+//           least wanted to work with them. Nick set out on getting his Masters in
+//           Business Administration as well as a Sports Marketing degree in hopes
+//           he could work with the athletes and be involved in motorsports. While
+//           going to school Nick came across a competition called GT Academy where
+//           Nissan and Gran Turismo (racing game) partner up to find the best
+//           virtual racing drivers in the world and turn them into real
+//           professional racing drivers.
+//         </Col>
+//       </Row>
+//     </Container>
+//   );
+// }
