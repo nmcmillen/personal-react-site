8ff4efeac3f47a1fb178d16ffd9b32b8e521adee
Nick McMillen <nick.mcmillen@student.awesomeinc.org>
2022-04-10 00:11:20 +0000

    contact section update test with gusto project footer section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -17,35 +17,77 @@ import portrait from "./images/nick-portrait.jpg";
 
 export default function Contact() {
   return (
-    <div>
-      <Container className="">
-        <Row className="justify-content-md-center">
-          <Col className="column-1" sm={3}>
-            <Image id="contact-headshot" src={portrait} />
-            <p>Web Developer</p>
-          </Col>
-          <Col className="column-2" sm={6}>
+    <footer className="page-footer font-small" id="footer">
+      <div className="container-fluid ">
+        <div className="row" id="row-edit">
+          <div
+            className="col-md-6 mt-3 pl-3 justify-content-center"
+            id="location"
+          >
+            <h5 className="text-uppercase">Nick McMillen</h5>
+            <p>Full Stack Web Developer</p>
+            <p>Awesome Inc. Bootcamp</p>
+            <h5 className="text-uppercase">Find Me</h5>
             <div className="social-icons">
               <FontAwesomeIcon icon={faTwitter} size="lg" />
               <FontAwesomeIcon icon={faYoutube} size="lg" />
               <FontAwesomeIcon icon={faGithub} size="lg" />
               <FontAwesomeIcon icon={faLinkedin} size="lg" />
             </div>
-            <strong>Awesome Inc. Bootcamp</strong>
-            <br />
-            348 E Main St.
-            <br />
-            Lexington, KY 40507
-            <br />
-            Phone: (123) 456-7890
-            <br />
-          </Col>
-        </Row>
-      </Container>
-    </div>
+            <p>
+              348 Main St.
+              <br />
+              Lexington, KY 40503 <br />
+              123-456-7890 <br />
+            </p>
+          </div>
+
+          <hr className="clearfix w-100 d-md-none pb-0 d-flex justify-content-center text-center" />
+
+          <div className="col-md-6">
+            <img
+              src={portrait}
+              className="img-fluid"
+              alt=""
+              style={{ maxWidth: "390px" }}
+            />
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 }
 
 // <FontAwesomeIcon icon={faCoffee} size="lg" />
 // <FontAwesomeIcon icon={faYoutube} size="lg" />
 // <FontAwesomeIcon icon={faGithub} size="3x" />
+
+//   <div>
+//     <Container className="">
+//       <Row className="justify-content-md-center">
+//         {/* LEFT COLUMN */}
+//         <Col className="column-1 align-items-center" sm={6}>
+//           <div className="social-icons">
+//             <FontAwesomeIcon icon={faTwitter} size="lg" />
+//             <FontAwesomeIcon icon={faYoutube} size="lg" />
+//             <FontAwesomeIcon icon={faGithub} size="lg" />
+//             <FontAwesomeIcon icon={faLinkedin} size="lg" />
+//           </div>
+//           <strong>Awesome Inc. Bootcamp</strong>
+//           <br />
+//           348 E Main St.
+//           <br />
+//           Lexington, KY 40507
+//           <br />
+//           Phone: (123) 456-7890
+//           <br />
+//         </Col>
+//         {/* RIGHT COLUMN */}
+//         <Col className="column-2" sm={3}>
+//           <Image id="contact-headshot" src={portrait} />
+//           <p>Web Developer</p>
+//         </Col>
+//       </Row>
+//     </Container>
+//   </div>
+// );
